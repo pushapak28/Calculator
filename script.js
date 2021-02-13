3d36881131debc940e69105c9fa938593ccc94b0
Pushapak Kumar <pp281997@gmail.com>
2021-02-13 14:14:39 +0530

    Decimal

--- script.js
+++ script.js
@@ -3,21 +3,40 @@ const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('cancel');
 
 function sendNumberValue(number) {
-    console.log(number);
+    // if th ecurrent value is 0 replace it , if not add number
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
 
-console.log(clearBtn);
-console.log(calculatorDisplay);
-console.log(inputBtns);
+
+// decimal function
+function addDecimal() {
+    // if ono decimal , add one 
+    if (!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
+}
+
+
+// console.log(clearBtn);
+// console.log(calculatorDisplay);
+// console.log(inputBtns);
 // Add event listners
 
 
 inputBtns.forEach((inputBtn) => {
     if (inputBtn.classList.length === 0) {
-        inputBtn.addEventListener('click',() => sendNumberValue(inputBtn.value));
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     } else if (inputBtn.classList.contains('operator')) {
-        inputBtn.addEventListener('click',() => sendNumberValue(inputBtn.value));
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     } else if (inputBtn.classList.contains('decimal')) {
-        inputBtn.addEventListener('click',() => sendNumberValue());
+        inputBtn.addEventListener('click', () => addDecimal());
     }
 });
+
+// Reset Display
+function resetAll() {
+    calculatorDisplay.textContent = '0';
+}
+// Event listener
+clearBtn.addEventListener('click', resetAll);
